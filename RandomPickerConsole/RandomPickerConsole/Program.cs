ac4d864e075ec4bb8afbaf6a592f978f9f8eaed6
Anders Feyling <feylinganders@gmail.com>
2018-11-08 21:31:56 +0100

    Read from file

---
+++ RandomPickerConsole/RandomPickerConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomPickerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string line;
+            string[] nameArray;
+            try
+            {
+                StreamReader sr = new StreamReader(@"../../names.txt.txt");
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read");
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
